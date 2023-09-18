0bbcf1d06b776ec9d29708153df20ba07ce17f79
user <katya.rotar02@gmail.com>
2023-09-18 23:05:42 +0300

    Task_5

---
+++ Task_5/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string date1 = Console.ReadLine();
+        string date2 = Console.ReadLine();
+        DateModifier dateModifier = new DateModifier();
+        Console.WriteLine(dateModifier.CalculateDate(date1, date2));
+
+    }
+}
+class DateModifier {
+    public int CalculateDate(string date_1, string date_2){
+        DateTime dateTime = DateTime.Parse(date_1);
+        DateTime dateTime_2 = DateTime.Parse(date_2);
+        TimeSpan difference_of_the_days = dateTime - dateTime_2;
+        return Math.Abs(difference_of_the_days.Days);
+        }
+}
